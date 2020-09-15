0b4a7570bcba610c29fe00dc69095ab8f1f93c8f
lookcw <lookwchristopher@gmail.com>
2020-09-15 00:18:02 -0400

    small cosmetic changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -32,7 +32,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} download className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a target="_blank" href={resumeDownload} download className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
